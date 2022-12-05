9ec4142b2439fd3e7ac304e1ea44c00b9e8cd473
AbigailLabanok <abigail.labanok@gmail.com>
2022-12-05 13:17:10 -0500

    k

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,15 +1,7 @@
 import "../styles/style.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
-import {menu} from "./menu";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello?????</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
 
-console.log(menu)
 
 //use extra js and css files to have certain code in place where it will be more easily accessible
