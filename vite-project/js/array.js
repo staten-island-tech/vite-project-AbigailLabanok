40265630f4925c1e13d7bd46e878ee0d7ab04e52
AbigailLabanok <abigail.labanok@gmail.com>
2022-12-06 13:16:51 -0500

    g

---
+++ vite-project/js/array.js
@@ -0,0 +1,52 @@
+const cardArr = [
+  {
+    name: " ",
+    price: 2.0,
+    img: "",
+    inStock: true,
+  },
+  {
+    name: "",
+    price: 5.0,
+    img: "",
+    inStock: true,
+  },
+  {
+    name: "",
+    price: 3.0,
+    img: "",
+    inStock: true,
+  },
+  {
+    name: "",
+    price: 4,
+    img: "",
+    inStock: true,
+  },
+  {
+    name: "",
+    price: 8.0,
+    img: "",
+    inStock: true,
+  },
+  {
+    name: "",
+    price: 10.0,
+    img: "",
+    inStock: true,
+  },
+  {
+    name: "",
+    price: 6,
+    img: "",
+    inStock: true,
+  },
+  {
+    name: "",
+    price: 3.0,
+    img: "",
+    inStock: true,
+  },
+];
+
+export { cardArr };
