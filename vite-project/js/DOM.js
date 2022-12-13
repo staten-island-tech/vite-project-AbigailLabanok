c7103e7a8945476f646f285a7169fd4b3b266397
AbigailLabanok <abigail.labanok@gmail.com>
2022-12-13 13:17:15 -0500

    f

---
+++ vite-project/js/DOM.js
@@ -0,0 +1,8 @@
+const DOMSelectors = {
+  blueBtn: document.getElementById("blue-btn"),
+  purpleBtn: document.getElementById("purple-btn"),
+  allBtn: document.getElementById("all-btn"),
+  display: document.querySelector(".display"),
+};
+
+export { DOMSelectors };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,7 +1,19 @@
 import "../styles/style.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+import { cardArr } from "./array";
+import {DOMSelectors} from "./DOM";
 
-
+cardArr.forEach((el) => {
+    console.log(el.name);
+    DOMSelectors.display.insertAdjacentHTML(
+        "beforeend",
+        `<div class="display-card">
+            <img class="display-img" src="${el.img}"/>
+            <h2 class="display-name">${el.name}</h2>
+            <h2 class="display-price">$${el.price}</h2>
+        </div>`
+    )
+})
 
 //use extra js and css files to have certain code in place where it will be more easily accessible
