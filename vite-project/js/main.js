4f61e388c9c5a754393e56bf9c9bf91aa30fbb54
AbigailLabanok <abigail.labanok@gmail.com>
2022-12-16 13:16:42 -0500

    s

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,23 +2,53 @@ import "../styles/style.css";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 import { cardArr } from "./array";
-import {DOMSelectors} from "./DOM";
+import { DOMSelectors } from "./DOM";
 
 cardArr.forEach((el) => {
-    DOMSelectors.display.insertAdjacentHTML(
-        "beforeend",
-        `<div class="display-card class="one">
+  DOMSelectors.display.insertAdjacentHTML(
+    "beforeend",
+    `<div class="display-card class="one">
             <img class="display-img" src="${el.img}"/>
             <h2 class="display-name">${el.name}</h2>
             <h3 class="display-price">${el.price}</h3>
         </div>`
-    )
+  );
 });
 
-DOMSelectors.blueBtn.addEventListener("click", blue)
+DOMSelectors.blueBtn.addEventListener("click", function () {
+  if (document.body.classList.contains("regularColor" || "purpleColor")) {
+    document.body.classList.add("blueColor");
+    document.body.classList.remove("regularColor" || "purpleColor");
+  } else {
+    document.body.classList.add("regularColor");
+    document.body.classList.add("blueColor");
+  }
+});
+
+DOMSelectors.purpleBtn.addEventListener("click", function () {
+  if (document.body.classList.contains("regularColor")) {
+    document.body.classList.add("purpleColor");
+    document.body.classList.remove("regularColor");
+  } else {
+    document.body.classList.add("regularColor");
+    document.body.classList.add("purpleColor");
+  }
+});
+
+/* DOMSelectors.blueBtn.addEventListener("click", blue)
 
 function blue() {
-    if (cardArr.mainColor);
-};
-
+    cardArr
+    .filter((color) => color.mainColor.includes("blue"))
+    .forEach((el) => {
+        DOMSelectors.display.insertAdjacentHTML(
+            "beforeend",
+            `<div class="display-card class="one">
+                <img class="display-img" src="${el.img}"/>
+                <h2 class="display-name">${el.name}</h2>
+                <h3 class="display-price">${el.price}</h3>
+            </div>`
+        )
+    })
+} */
 //use extra js and css files to have certain code in place where it will be more easily accessible
